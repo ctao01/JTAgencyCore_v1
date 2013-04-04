4c8ea725d0f9859ea3f090292cfccfdbd333b786
ctao <ctao01@students.poly.edu>
2013-04-04 08:27:27 -0500

    rotation features with stack view controllers (not neat)

---
+++ AgencyCoreMobile_v1002J/Classes/ACNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  ACNavigationController.h
+//  AgencyCoreMobile_v1002J
+//
+//  Created by Joy Tao on 4/3/13.
+//  Copyright (c) 2013 Joy Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACNavigationController : UINavigationController
+
+@end
---
+++ AgencyCoreMobile_v1002J/Classes/ACNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  ACNavigationController.m
+//  AgencyCoreMobile_v1002J
+//
+//  Created by Joy Tao on 4/3/13.
+//  Copyright (c) 2013 Joy Tao. All rights reserved.
+//
+
+#import "ACNavigationController.h"
+
+@interface ACNavigationController ()
+
+@end
+
+@implementation ACNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
